8eeef28dbaa4d1034f1761f6e1f47be8da57e7a5
YiLin Choi <yilinchoi@hotmail.com>
2026-04-28 11:39:46 -0400

    m2 print fat

--- nyufile.c
+++ nyufile.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+typedef struct __attribute__((packed)) {
+    uint8_t  BS_jmpBoot[3];
+    uint8_t  BS_OEMName[8];
+    uint16_t BPB_BytsPerSec;
+    uint8_t  BPB_SecPerClus;
+    uint16_t BPB_RsvdSecCnt;
+    uint8_t  BPB_NumFATs;
+    uint16_t BPB_RootEntCnt;
+    uint16_t BPB_TotSec16;
+    uint8_t  BPB_Media;
+    uint16_t BPB_FATSz16;
+    uint16_t BPB_SecPerTrk;
+    uint16_t BPB_NumHeads;
+    uint32_t BPB_HiddSec;
+    uint32_t BPB_TotSec32;
+    uint32_t BPB_FATSz32;
+    uint16_t BPB_ExtFlags;
+    uint16_t BPB_FSVer;
+    uint32_t BPB_RootClus;
+    uint16_t BPB_FSInfo;
+    uint16_t BPB_BkBootSec;
+    uint8_t  BPB_Reserved[12];
+    uint8_t  BS_DrvNum;
+    uint8_t  BS_Reserved1;
+    uint8_t  BS_BootSig;
+    uint32_t BS_VolID;
+    uint8_t  BS_VolLab[11];
+    uint8_t  BS_FilSysType[8];
+} BootSector;
 
 static void print_usage(const char *prog_name) {
     printf("Usage: %s disk <options>\n", prog_name);
@@ -7,6 +41,21 @@ static void print_usage(const char *prog_name) {
     printf("  -l                     List the root directory.\n");
     printf("  -r filename [-s sha1]  Recover a contiguous file.\n");
     printf("  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
+}
+
+static void cmd_info(const char *disk_path) {
+    int fd = open(disk_path, O_RDONLY);
+    struct stat st;
+    fstat(fd, &st);
+    BootSector *bs = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    printf("Number of FATs = %u\n",               bs->BPB_NumFATs);
+    printf("Number of bytes per sector = %u\n",   bs->BPB_BytsPerSec);
+    printf("Number of sectors per cluster = %u\n", bs->BPB_SecPerClus);
+    printf("Number of reserved sectors = %u\n",   bs->BPB_RsvdSecCnt);
+
+    munmap(bs, st.st_size);
+    close(fd);
 }
 
 int main(int argc, char *argv[]) {
@@ -62,6 +111,10 @@ int main(int argc, char *argv[]) {
     if (recover_any_arg != NULL && sha1_arg == NULL) {
         print_usage(prog_name);
         return 1;
+    }
+
+    if (info_flag) {
+        cmd_info(argv[1]);
     }
 
     return 0;
